f66e3b7da267fc325e7cfdee9c5fa88a6539e88b
Sanjeeb Dey <sanjeebdey1@gmail.com>
2023-09-29 12:38:36 -0700

    Added zsh

--- kde/install.sh
+++ kde/install.sh
@@ -1,5 +1,4 @@
 sudo add-apt-repository ppa:touchegg/stable
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
-sudo apt-get install sublime-text touchegg
-
+sudo apt-get install sublime-text touchegg zsh -y
