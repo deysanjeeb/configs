5fdc2d1b35c5b276316443a54ebc504ad4850201
skd <Sanjeeb Dey>
2023-09-29 12:23:19 -0700

    Added sublime

--- kde/install.sh
+++ kde/install.sh
@@ -1,3 +1,5 @@
 sudo add-apt-repository ppa:touchegg/stable
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
-sudo apt install touchegg
+sudo apt-get install sublime-text touchegg
+
