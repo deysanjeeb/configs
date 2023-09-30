e41eabc128718a1154b54e3249b9a633bf957732
Sanjeeb Dey <sanjeebdey1@gmail.com>
2023-09-29 20:30:59 -0700

    Fixed typo

--- kde/install.sh
+++ kde/install.sh
@@ -1,7 +1,7 @@
 sudo add-apt-repository ppa:touchegg/stable
 command -v subl > /dev/null || {
     echo "Installing Sublime Text"
-    wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gp>
+    wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gppg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
     sudo apt update
     sudo apt-get install sublime-text -y
