0a2e36a33b24a7c2c5f919a727ddce94b3d6a56c
Sanjeeb Dey <sanjeebdey1@gmail.com>
2023-09-29 20:28:57 -0700

    Added curl

--- kde/install.sh
+++ kde/install.sh
@@ -1,7 +1,16 @@
 sudo add-apt-repository ppa:touchegg/stable
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+command -v subl > /dev/null || {
+    echo "Installing Sublime Text"
+    wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gp>
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+    sudo apt update
+    sudo apt-get install sublime-text -y
+}
 sudo apt update
-sudo apt-get install sublime-text touchegg zsh -y
+sudo apt-get install touchegg zsh -y
+command -v curl > /dev/null || {
+    echo "Installing curl"
+    sudo apt-get install curl
+}
 echo "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
