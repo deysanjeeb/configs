36aec6036edb123a54f53232b03b5478289eb0c9
Sanjeeb Dey <sanjeebdey1@gmail.com>
2023-09-29 20:16:45 -0700

    oh my zsh

--- kde/install.sh
+++ kde/install.sh
@@ -1,4 +1,7 @@
 sudo add-apt-repository ppa:touchegg/stable
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
 sudo apt-get install sublime-text touchegg zsh -y
+echo "Installing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
